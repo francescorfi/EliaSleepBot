c3471ee3eb828655f4c64520d2d4fc20a19c0068
Francesc Orfi <francescorfi@gmail.com>
2023-07-31 20:38:41 +0200

    print error message

---
+++ set_hook.php
@@ -0,0 +1,22 @@
+<?php
+require 'vendor/autoload.php';
+require 'config.php';
+
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\TelegramLog;
+use Longman\TelegramBot\Exception\TelegramException;
+
+try {
+    // Create Telegram API object
+    $telegram = new Telegram($settings['api_key'], $settings['bot_username']);
+
+    // Set webhook
+    $result = $telegram->setWebhook($settings['hook_url']);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (TelegramException $e) {
+    // Log telegram errors
+    TelegramLog::error($e);
+    echo $e;
+}
