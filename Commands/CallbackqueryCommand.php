0b4fe85998099adfcd2bbe30f125dcda915d3c05
Francesc Orfi <francescorfi@gmail.com>
2023-07-31 20:46:58 +0200

    fix problem with callbackquery

--- Commands/CallbackqueryCommand.php
+++ Commands/CallbackqueryCommand.php
@@ -12,15 +12,17 @@ class CallbackqueryCommand extends SystemCommand
     protected $description = 'Handle callback queries';
     protected $version = '1.0.0';
 
-    public function execute()
+    public function execute(): Longman\TelegramBot\Entities\ServerResponse
     {
         $callback_query = $this->getCallbackQuery();
         $callback_query_id = $callback_query->getId();
         $callback_data = $callback_query->getData();
-
+    
         $message = $callback_query->getMessage();
         $chat_id = $message->getChat()->getId();
-
+    
+        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);  // Abrimos la conexión aquí
+    
         // Manejo de los comandos configurados en el teclado inline
         switch ($callback_data) {
             case 'command_new_message':
@@ -34,18 +36,17 @@ class CallbackqueryCommand extends SystemCommand
                     $message_id = substr($callback_data, 5);
             
                     // Guardar el estado en la base de datos (en este ejemplo, lo guardamos en una nueva tabla llamada 'user_state')
-                    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
                     $stmt = $mysqli->prepare("INSERT INTO user_state (chat_id, state, message_id) VALUES (?, 'awaiting_new_message', ?)");
                     $stmt->bind_param('ii', $chat_id, $message_id);
                     $stmt->execute();
                     $stmt->close();
-                    $mysqli->close();
                 
                     // Enviar un mensaje al usuario pidiendo el nuevo mensaje
                     $data = [
                         'chat_id' => $chat_id,
                         'text'    => 'Por favor, escribe el nuevo mensaje',
                     ];
+                    $mysqli->close(); // Cerramos la conexión aquí
                     return Request::sendMessage($data);
                 } else if (strpos($callback_data, 'delete_') === 0) {
                     $message_id = substr($callback_data, 7);
@@ -53,17 +54,16 @@ class CallbackqueryCommand extends SystemCommand
                     $stmt->bind_param('si', $message_id, $chat_id);
                     $stmt->execute();
                     $stmt->close();
-                    $mysqli->close();
                 
                     $data = [
                         'callback_query_id' => $callback_query_id,
                         'text'              => 'Mensaje eliminado con éxito',
                         'show_alert'        => true,  // Mostrar un alerta en lugar de una notificación.
                     ];
-                    
+                    $mysqli->close(); // Cerramos la conexión aquí
                     return Request::answerCallbackQuery($data);
                 }
                 break;
         }
-    }
+    }    
 }
