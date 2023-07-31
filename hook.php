7c810d15ae7fe2ca960a2778ddea2d1cac3fc660
Francesc Orfi <francescorfi@gmail.com>
2023-07-31 20:59:06 +0200

    fix

--- hook.php
+++ hook.php
@@ -4,10 +4,7 @@ require 'config.php';
 
 use Longman\TelegramBot\Telegram;
 use Longman\TelegramBot\TelegramLog;
-use Longman\TelegramBot\Entities\Update;
 use Longman\TelegramBot\Exception\TelegramException;
-
-use Commands\MessageCommand;
 
 try {
     // Create Telegram API object
@@ -18,17 +15,6 @@ try {
 
     // Handle telegram webhook request
     $telegram->handle();
-
-    // Get the update object
-    $update = $telegram->getWebhookUpdate();
-
-    // Check if the update contains a text message
-    if ($update->getMessage() && !$update->getMessage()->isCommand()) {
-        // Instantiate and execute the MessageCommand
-        $command = new MessageCommand($telegram, $update);
-        $command->preExecute();
-        $command->execute();
-    }
 
 } catch (TelegramException $e) {
     // Log telegram errors
