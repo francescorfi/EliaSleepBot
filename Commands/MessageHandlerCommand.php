bc5bf80093ae87f29374c59383c16a73b8b21711
Francesc Orfi <francescorfi@gmail.com>
2023-08-22 12:53:00 +0200

    testing

--- Commands/MessageHandlerCommand.php
+++ Commands/MessageHandlerCommand.php
@@ -14,6 +14,7 @@ class MessageHandlerCommand extends SystemCommand
 
     public function execute()
     {
+        TelegramLog::error("MessageHandlerCommand is being executed!");
 
         try {
             $message = $this->getMessage();
